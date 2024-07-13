cb7ca04aeb87723d17754fa5601ff41ef22b27bb
Dmitriy <dmitriy.slinkovenko@gmail.com>
2024-07-13 16:05:40 -0400

    Bugfix

--- routes/cocktails.js
+++ routes/cocktails.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const router = express.Router();
 
-const { createItem, getItems } = require("../controllers/news");
+const { createItem, getItems } = require("../controllers/cocktail");
 const { validateCardBody } = require("../middlewares/validation");
 
 router.get("/", getItems);
